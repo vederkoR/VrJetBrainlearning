c49ae73b9ba91757ffd16b06c16f4d10f8e91e7b
vederko <reshviktor1@gmail.com>
2022-03-31 20:53:02 +0300

    hangman stage 5

--- python basics/hangman.py
+++ python basics/hangman.py
@@ -1,12 +1,21 @@
 import random
 
+num_of_attempts = 8
 print("H A N G M A N")
-
+print()
 list_ = ["python", "java", "swift", "javascript"]
 random.shuffle(list_)
 word = list_[0]
-answer = input(f"Guess the word {word[0 : 3]}" + '-'*(len(word)-3) + ":")
-if answer == word:
-    print("You survived!")
-else:
-    print("You lost!")
+word_ = "-" * (len(word))
+while num_of_attempts != 0:
+    print(word_)
+    answer = input("Input a letter:")
+    num_of_attempts -= 1
+    if answer in word:
+        for i in range(len(word)):
+            if word[i] == answer:
+                word_ = word_[:i] + answer + word_[i + 1:]
+    else:
+        print("That letter doesn't appear in the word.")
+    print()
+print("Thanks for playing!")
