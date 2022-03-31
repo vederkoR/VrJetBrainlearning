f581ad7cfda8e9719fc24d9ab56ed5b1a39a560a
vederko <reshviktor1@gmail.com>
2022-03-31 19:45:36 +0300

    hangman stage 4

--- python basics/hangman.py
+++ python basics/hangman.py
@@ -4,9 +4,9 @@ print("H A N G M A N")
 
 list_ = ["python", "java", "swift", "javascript"]
 random.shuffle(list_)
-
-answer = input("Guess the word:")
-if answer == list_[0]:
+word = list_[0]
+answer = input(f"Guess the word {word[0 : 3]}" + '-'*(len(word)-3) + ":")
+if answer == word:
     print("You survived!")
 else:
     print("You lost!")
