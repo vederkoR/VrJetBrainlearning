df969109fe726c9efe81b9d7bcf4b732383a9231
vederko <reshviktor1@gmail.com>
2022-04-10 20:23:09 +0300

    add stage 3 Text Generator

--- python core/text_generator.py
+++ python core/text_generator.py
@@ -7,17 +7,30 @@ with open(filename, "r", encoding="utf-8") as file:
     text = file.read()
     tokens = WhitespaceTokenizer().tokenize(text)
     bigrm = list(nltk.bigrams(tokens))
-    print("Number of bigrams:", len(bigrm))
-    print()
+    dict_words = {}
+    for pair in bigrm:
+        dict_words.setdefault(pair[0], {})
+        dict_words[pair[0]].setdefault(pair[1], 0)
+        dict_words[pair[0]][pair[1]] += 1
     while True:
-        token_num = input()
-        if token_num == "exit":
+        key_name = input()
+        if key_name == "exit":
             break
+        print("Head:", key_name)
         try:
-            print(f"Head: {bigrm[int(token_num)][0]}    Tail: {bigrm[int(token_num)][1]}")
-        except TypeError:
-            print("Type Error. Please input an integer.")
-        except IndexError:
-            print("Index Error. Please input an integer that is in the range of the corpus.")
-        except ValueError:
-            print("Value Error. Please input an integer from 0 to corpus size")
+            for key, value in dict_words[key_name].items():
+                print(f"Tail: {key} Count: {value}")
+        except KeyError:
+            print("Key Error. The requested word is not in the model. Please input another word.")
+
+
+
+
+        # try:
+        #     print(f"Head: {bigrm[int(token_num)][0]}    Tail: {bigrm[int(token_num)][1]}")
+        # except TypeError:
+        #     print("Type Error. Please input an integer.")
+        # except IndexError:
+        #     print("Index Error. Please input an integer that is in the range of the corpus.")
+        # except ValueError:
+        #     print("Value Error. Please input an integer from 0 to corpus size")
