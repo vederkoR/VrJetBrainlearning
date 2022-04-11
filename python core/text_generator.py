e23c0761254002a8c19658530873eeef7a8477d1
vederko <reshviktor1@gmail.com>
2022-04-11 20:08:24 +0300

    text generator stage 5

--- python core/text_generator.py
+++ python core/text_generator.py
@@ -1,4 +1,5 @@
 import random
+import re
 import nltk
 from nltk.tokenize import WhitespaceTokenizer
 
@@ -13,12 +14,17 @@ with open(filename, "r", encoding="utf-8") as file:
         dict_words.setdefault(pair[0], {})
         dict_words[pair[0]].setdefault(pair[1], 0)
         dict_words[pair[0]][pair[1]] += 1
+    tokens_cap = [x for x in tokens if (x[0].isupper() and bool(re.fullmatch("[A-z'-]+", x)))]
     for _ in range(0, 10):
-        word = random.choice(tokens)
+        word = random.choice(tokens_cap)
         phrase = ""
         phrase += word
         word_next = word
-        for i in range(0, 9):
+        for i in range(0, 4):
+            word_next = \
+                random.choices(list(dict_words[word_next].keys()), weights=list(dict_words[word_next].values()), k=1)[0]
+            phrase += " " + word_next
+        while ('.' not in word_next) and ('!' not in word_next) and ('?' not in word_next):
             word_next = \
                 random.choices(list(dict_words[word_next].keys()), weights=list(dict_words[word_next].values()), k=1)[0]
             phrase += " " + word_next
