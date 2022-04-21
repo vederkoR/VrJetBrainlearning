80769863d7b6011ac632c2266283e2b7aa988520
vederko <reshviktor1@gmail.com>
2022-04-21 11:42:42 +0300

    flashcards 3

--- python core/flashcards.py
+++ python core/flashcards.py
@@ -1,8 +1,13 @@
 # Write your code here
-term = input()
-question = input()
-answer = input()
-if question == answer:
-    print("Your answer is right!")
-else:
-    print("Your answer is wrong...")
+cards = dict()
+num = int(input("Input the number of cards:\n"))
+for i in range(num):
+    term =input(f"The term for card #{i + 1}:\n")
+    definition = input(f"The definition for card #{i + 1}:\n")
+    cards[term] = definition
+for chosen in cards.keys():
+    answer = input(f'Print the definition of "{chosen}":\n')
+    if cards[chosen] == answer:
+        print("Correct!")
+    else:
+        print(f'Wrong. The right answer is "{cards[chosen]}".')
