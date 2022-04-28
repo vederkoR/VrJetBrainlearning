65de337214a974fc68fc99dbe811e32c8b62d3f2
vederko <reshviktor1@gmail.com>
2022-04-28 12:44:33 +0300

    password hacker stage 2

--- python core/password_hacker.py
+++ python core/password_hacker.py
@@ -1,15 +1,24 @@
+import itertools
 import socket
+import string
 import sys
 
+symbols = list(string.ascii_lowercase + string.digits)
 args = sys.argv
 ip_address = args[1]
 port = int(args[2])
-password = args[3]
+flag = False
 with socket.socket() as admin_socket:
     admin_socket.connect((ip_address, port))
-    admin_socket.send(password.encode())
-
-    buffer_size = 1024
-    message = admin_socket.recv(buffer_size)
-    print(message.decode())
-
+    for i in range(1, 10):
+        for x in itertools.product(symbols, repeat=i):
+            tried = "".join(x)
+            admin_socket.send(tried.encode())
+            buffer_size = 1024
+            message = admin_socket.recv(buffer_size)
+            if message.decode() == "Connection success!":
+                flag = True
+                print(tried)
+                break
+        if flag:
+            break
