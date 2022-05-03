c043f5844f0ae2fba415870de04b32ec721dfa11
vederko <reshviktor1@gmail.com>
2022-05-03 23:47:00 +0300

    translator stage 1

---
+++ python core/translator.py
@@ -0,0 +1,6 @@
+print('Type "en" if you want to translate from French into English, or "fr" if you want to /'
+      'translate from English into French:')
+choice = input()
+print('Type the word you want to translate:')
+word = input()
+print(f'You chose "{choice}" as the language to translate "{word}"')
