02cd1384c6d216ada608968cb439e48314cf7064
vederko <reshviktor1@gmail.com>
2022-05-14 14:27:56 +0300

    translator stage 5

--- python core/translator.py
+++ python core/translator.py
@@ -2,47 +2,104 @@ import requests
 
 from bs4 import BeautifulSoup
 
-print('Hello, welcome to the translator. Translator supports:')
-lang_pool = {"1": "Arabic", "2": "German", "3": "English", "4": "Spanish", "5": "French", "6": "Hebrew",
-             "7": "Japanese", "8": "Dutch", "9": "Polish", "10": "Portuguese", "11": "Romanian",
-             "12": "Russian", "13": "Turkish"}
-for k, v in lang_pool.items():
-    print(f"{k}. {v}")
-print("Type the number of your language:")
-language_from_choice = input()
-print("Type the number of language you want to translate to:")
-language_to_choice = input()
-print('Type the word you want to translate:')
-word = input()
-translation_pair = f'{lang_pool[language_from_choice].lower()}-{lang_pool[language_to_choice].lower()}'
 
-url = 'https://context.reverso.net/translation/' + translation_pair + '/' + word
-headers = {'User-Agent': 'Mozilla/5.0'}
-r = requests.get(url, headers=headers)
-while str(r.status_code) != "200":
-    r = requests.get(url, headers=headers)
-soup = BeautifulSoup(r.content, "html.parser")
-print()
-print(lang_pool[language_to_choice], "Translations:")
-examples = []
-all_ = soup.find_all('div', class_="example")
-for one in all_:
-    examples.append(one.text.strip().replace("\n\n\n\n\r\n          ", "\n"))
+class Translator:
+    """This class represents a translator which is able to translate to 13 most popular languages \
+     with multiple examples"""
 
-words = []
+    lang_pool = {"1": "Arabic", "2": "German", "3": "English", "4": "Spanish", "5": "French", "6": "Hebrew",
+                 "7": "Japanese", "8": "Dutch", "9": "Polish", "10": "Portuguese", "11": "Romanian",
+                 "12": "Russian", "13": "Turkish"}
+    url_base = 'https://context.reverso.net/translation/'
+    headers = {'User-Agent': 'Mozilla/5.0'}
 
-tr_words = soup.find_all("span", class_="display-term")
-for tr_word in tr_words:
-    words.append(tr_word.text.strip())
+    def __init__(self):
+        self.url = None
+        self.index_to = None
+        self.index_from = None
+        self.to_translate = None
+        self.translation_pair = None
+        self.string_to_save = ''
 
-for single_word in words:
-    print(single_word)
+    def run(self):
+        print('Hello, welcome to the translator. Translator supports:')
+        for k, v in Translator.lang_pool.items():
+            print(f"{k}. {v}")
+        self.params_set()
+        if self.index_to == "0":
+            self.case_zero_action()
+        else:
+            self.case_not_zero_action()
+        path = self.to_translate + ".txt"
+        with open(path, mode='w') as file:
+            file.write(self.string_to_save)
 
-print()
-print(lang_pool[language_to_choice], "Examples:")
-j = 0
-for example in examples:
-    if j > 0:
-        print()
-    print(example)
-    j += 1
+    def params_set(self):
+        print("Type the number of your language:")
+        self.index_from = input()
+        print("Type the number of a language you want to translate to or '0' to translate to all languages:")
+        self.index_to = input()
+        print('Type the word you want to translate:')
+        self.to_translate = input()
+
+    @staticmethod
+    def connection(url_inner, headers_inner):
+        r = requests.get(url_inner, headers=headers_inner)
+        while str(r.status_code) != "200":
+            r = requests.get(url_inner, headers=headers_inner)
+        soup = BeautifulSoup(r.content, "html.parser")
+        return soup
+
+    @staticmethod
+    def lang_title_printer(lang_to):
+        return f"{lang_to} Translations:"
+
+    @staticmethod
+    def example_title_printer(lang_to):
+        return f"{lang_to} Examples:"
+
+    def print_s(self, *args):
+        for arg in args:
+            self.string_to_save += arg
+        self.string_to_save += '\n'
+        print(*args)
+
+    def case_zero_action(self):
+        for lang in Translator.lang_pool.values():
+            if lang == Translator.lang_pool[self.index_from]:
+                continue
+            soup = self.soup_set(lang)
+            self.print_s(Translator.lang_title_printer(lang))
+            self.print_s(soup.find_all("span", class_="display-term")[0].text.strip())
+            self.print_s()
+            self.print_s(Translator.example_title_printer(lang))
+            self.print_s(soup.find_all('div', class_="example")[0].text.strip().replace("\n\n\n\n\r\n          ", "\n")
+                         .replace("\n\n\n\n\n", "\n"))
+
+    def case_not_zero_action(self):
+        soup = self.soup_set(Translator.lang_pool[self.index_to])
+        self.print_s(Translator.lang_title_printer(Translator.lang_pool[self.index_to]))
+
+        words = soup.find_all("span", class_="display-term")
+        for word in words:
+            self.print_s(word.text.strip())
+        self.print_s()
+
+        self.print_s(Translator.example_title_printer(Translator.lang_pool[self.index_to]))
+        all_examples = soup.find_all('div', class_="example")
+        flag = False
+        for example in all_examples:
+            if flag:
+                self.print_s()
+            self.print_s(example.text.strip().replace("\n\n\n\n\r\n          ", "\n").replace("\n\n\n\n\n", "\n"))
+            flag = True
+
+    def soup_set(self, lang_to):
+        self.translation_pair = \
+            f'{Translator.lang_pool[self.index_from].lower()}-{lang_to.lower()}'
+        self.url = Translator.url_base + self.translation_pair + '/' + self.to_translate
+        return Translator.connection(self.url, Translator.headers)
+
+
+translator = Translator()
+translator.run()
