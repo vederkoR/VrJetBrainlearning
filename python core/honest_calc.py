854269aad9193ad67b2311ea4039329b5d772a11
vederko <reshviktor1@gmail.com>
2022-05-16 22:08:33 +0300

    honest calculator stage 2

--- python core/honest_calc.py
+++ python core/honest_calc.py
@@ -2,6 +2,7 @@ class RudeCommenter:
     msg_0 = "Enter an equation"
     msg_1 = "Do you even know what numbers are? Stay focused!"
     msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
+    msg_3 = "Yeah... division by zero. Smart move..."
 
     def __init__(self):
         self.equation = None
@@ -19,12 +20,26 @@ class RudeCommenter:
             if self.sign not in r"+-*/":
                 print(RudeCommenter.msg_2)
                 continue
+            if self.sign == "/" and float(self.second_var) == 0:
+                print(RudeCommenter.msg_3)
+                continue
+            print(self.operation())
             break
 
     def set_equation(self):
         print(RudeCommenter.msg_0)
         self.equation = input()
         self.first_var, self.sign, self.second_var = self.equation.split()
+
+    def operation(self):
+        if self.sign == "/":
+            return float(self.first_var) / float(self.second_var)
+        if self.sign == "*":
+            return float(self.first_var) * float(self.second_var)
+        if self.sign == "+":
+            return float(self.first_var) + float(self.second_var)
+        else:
+            return float(self.first_var) - float(self.second_var)
 
 
 def main():
@@ -34,3 +49,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
