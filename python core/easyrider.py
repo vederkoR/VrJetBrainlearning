12dc835772764b695889b494cea052a5590abf3b
vederko <reshviktor1@gmail.com>
2022-05-20 21:56:01 +0300

    easy bus driver stage 2

--- python core/easyrider.py
+++ python core/easyrider.py
@@ -5,10 +5,10 @@ json_str = input()
 standards = {
     "bus_id": list(range(10_000)),
     "stop_id": list(range(1000)),
-    "stop_name": re.compile(r"\w{2,}"),
+    "stop_name": re.compile(r"[A-Z][\w ]*?\s(Road|Avenue|Boulevard|Street)"),
     "next_stop": list(range(1000)),
     "stop_type": {"S", "O", "F", ""},
-    "a_time": re.compile(r"\d{2}:\d{2}")
+    "a_time": re.compile(r"([0-1]\d|2[0-3]):[0-5]\d")
 }
 bus_id_er = 0
 stop_id_er = 0
@@ -25,8 +25,9 @@ for bus in bus_json_data:
     if type(bus["stop_name"]) != str:
         stop_name_er += 1
     else:
-        if not (re.match(standards["stop_name"], bus["stop_name"])):
+        if not (re.fullmatch(standards["stop_name"], bus["stop_name"])):
             stop_name_er += 1
+            # print(bus["stop_name"])
     if bus["next_stop"] not in standards["next_stop"]:
         next_stop_er += 1
     if bus["stop_type"] not in standards["stop_type"]:
@@ -34,13 +35,14 @@ for bus in bus_json_data:
     if type(bus["a_time"]) != str:
         a_time_er += 1
     else:
-        if not re.match(standards["a_time"], bus["a_time"]):
+        if not re.fullmatch(standards["a_time"], bus["a_time"]):
             a_time_er += 1
-total_er = bus_id_er + stop_id_er + stop_name_er + next_stop_er + stop_type_er + a_time_er
+total_er = stop_name_er + stop_type_er + a_time_er
 print(f"Type and required field validation: {total_er} errors")
-print(f"bus_id: {bus_id_er}")
-print(f"stop_id: {stop_id_er}")
+# print(f"bus_id: {bus_id_er}")
+# print(f"stop_id: {stop_id_er}")
 print(f"stop_name: {stop_name_er}")
-print(f"next_stop: {next_stop_er}")
+# print(f"next_stop: {next_stop_er}")
 print(f"stop_type: {stop_type_er}")
 print(f"a_time: {a_time_er}")
+
