4e615211cbce557d9c34f8abe1e22fc6ab5e552e
vederko <reshviktor1@gmail.com>
2022-06-01 11:44:18 +0300

    coffee machine stage 2

--- python core/coffee_machine.py
+++ python core/coffee_machine.py
@@ -1,9 +1,6 @@
-print("""
-Starting to make a coffee
-Grinding coffee beans
-Boiling water
-Mixing boiled water with crushed coffee beans
-Pouring coffee into the cup
-Pouring some milk into the cup
-Coffee is ready!
-""")
+print("Write how many cups of coffee you will need:")
+num_cups = int(input())
+print(f"""For {num_cups} cups of coffee you will need:
+{num_cups * 200} ml of water
+{num_cups * 50} ml of milk
+{num_cups * 15} g of coffee beans""")
