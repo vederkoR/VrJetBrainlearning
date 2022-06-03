97ae4a7cf2c0a08c1e3d23f019b39488b28f5f00
vederko <reshviktor1@gmail.com>
2022-06-03 20:10:01 +0300

    text-based browser stage 4

--- python core/text_base_browser.py
+++ python core/text_base_browser.py
@@ -1,41 +1,7 @@
 import collections
 import os
 import sys
-
-nytimes_com = '''
-This New Liquid Is Magnetic, and Mesmerizing
-
-Scientists have created "soft" magnets that can flow 
-and change shape, and that could be a boon to medicine 
-and robotics. (Source: New York Times)
-
-
-Most Wikipedia Profiles Are of Men. This Scientist Is Changing That.
-
-Jessica Wade has added nearly 700 Wikipedia biographies for
- important female and minority scientists in less than two 
- years.
-
-'''
-
-bloomberg_com = '''
-The Space Race: From Apollo 11 to Elon Musk
-
-It's 50 years since the world was gripped by historic images
- of Apollo 11, and Neil Armstrong -- the first man to walk 
- on the moon. It was the height of the Cold War, and the charts
- were filled with David Bowie's Space Oddity, and Creedence's 
- Bad Moon Rising. The world is a very different place than 
- it was 5 decades ago. But how has the space race changed since
- the summer of '69? (Source: Bloomberg)
-
-
-Twitter CEO Jack Dorsey Gives Talk at Apple Headquarters
-
-Twitter and Square Chief Executive Officer Jack Dorsey 
- addressed Apple Inc. employees at the iPhone maker’s headquarters
- Tuesday, a signal of the strong ties between the Silicon Valley giants.
-'''
+import requests
 
 
 def file_opener(dir_to, file_in):
@@ -54,23 +20,19 @@ except FileExistsError:
 # write your code here
 while True:
     command = input()
-    if command.split(".")[0] == "bloomberg":
-        if os.access(f'{dir_}/bloomberg', os.F_OK):
-            file_opener(dir_, 'bloomberg')
+    if '.' in command:
+        if 'http' not in command:
+            command = 'https://' + command
+        name = command[8:].split(".")[0]
+        if os.access(f'{dir_}/{name}', os.F_OK):
+            file_opener(dir_, name)
         else:
-            with open(f'{dir_}/bloomberg', mode='w', encoding='utf-8') as file:
-                file.write(bloomberg_com)
-            print(bloomberg_com)
-        command_stack.append("bloomberg")
-    elif command.split(".")[0] == "nytimes":
-        last_command = "nytimes"
-        if os.access(f'{dir_}/nytimes', os.F_OK):
-            file_opener(dir_, 'nytimes')
-        else:
-            with open(f'{dir_}/nytimes', mode='w', encoding='utf-8') as file:
-                file.write(nytimes_com)
-            print(nytimes_com)
-        command_stack.append("nytimes")
+            r = requests.get(command)
+            with open(f'{dir_}/{name}', mode='w', encoding='utf-8') as file:
+                file.write(r.text)
+            print(r.text)
+        command_stack.append(command)
+
     elif command == "exit":
         break
     elif command == 'back':
