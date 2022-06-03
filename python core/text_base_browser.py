bfd1706226f93ee9d14d1cc7f958275961a4378b
vederko <reshviktor1@gmail.com>
2022-06-03 19:37:24 +0300

    text-based browser stage 3

--- python core/text_base_browser.py
+++ python core/text_base_browser.py
@@ -1,3 +1,4 @@
+import collections
 import os
 import sys
 
@@ -36,6 +37,14 @@ Twitter and Square Chief Executive Officer Jack Dorsey
  Tuesday, a signal of the strong ties between the Silicon Valley giants.
 '''
 
+
+def file_opener(dir_to, file_in):
+    with open(f'{dir_to}/{file_in}', mode='r', encoding='utf-8') as file_:
+        print(file_.read())
+
+
+last_command = ''
+command_stack = collections.deque()
 args = sys.argv
 dir_ = args[1]
 try:
@@ -47,21 +56,27 @@ while True:
     command = input()
     if command.split(".")[0] == "bloomberg":
         if os.access(f'{dir_}/bloomberg', os.F_OK):
-            with open(f'{dir_}/bloomberg', mode='r', encoding='utf-8') as file:
-                print(file.read())
+            file_opener(dir_, 'bloomberg')
         else:
             with open(f'{dir_}/bloomberg', mode='w', encoding='utf-8') as file:
                 file.write(bloomberg_com)
             print(bloomberg_com)
+        command_stack.append("bloomberg")
     elif command.split(".")[0] == "nytimes":
+        last_command = "nytimes"
         if os.access(f'{dir_}/nytimes', os.F_OK):
-            with open(f'{dir_}/nytimes', mode='r', encoding='utf-8') as file:
-                print(file.read())
+            file_opener(dir_, 'nytimes')
         else:
             with open(f'{dir_}/nytimes', mode='w', encoding='utf-8') as file:
                 file.write(nytimes_com)
             print(nytimes_com)
+        command_stack.append("nytimes")
     elif command == "exit":
         break
+    elif command == 'back':
+        if len(command_stack) > 1:
+            command_stack.pop()
+            file_opener(dir_, command_stack[-1])
+
     else:
         print("Error: Incorrect URL")
