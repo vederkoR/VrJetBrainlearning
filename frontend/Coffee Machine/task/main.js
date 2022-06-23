5dc7ae64ce767016c5196a74232d2ad105b28a09
vederko <reshviktor1@gmail.com>
2022-06-23 15:15:28 +0300

    frontend CoffeeMachine 1/6

---
+++ frontend/Coffee Machine/task/main.js
@@ -0,0 +1,12 @@
+// Use "input()" to input a line from the user
+// Use "input(str)" to print some text before requesting input
+// You will need this in the following stages
+const input = require('sync-input')
+
+console.log("Starting to make a coffee")
+console.log("Grinding coffee beans")
+console.log("Boiling water")
+console.log("Mixing boiled water with crushed coffee beans")
+console.log("Pouring coffee into the cup")
+console.log("Pouring some milk into the cup")
+console.log("Coffee is ready!")
