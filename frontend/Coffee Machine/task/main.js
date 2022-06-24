2e481f01c44ebbf4b3636023efb030bb3e8d1c53
vederko <reshviktor1@gmail.com>
2022-06-24 20:34:11 +0300

    frontend CoffeeMachine stage 5/6

--- frontend/Coffee Machine/task/main.js
+++ frontend/Coffee Machine/task/main.js
@@ -1,48 +1,71 @@
 const input = require('sync-input')
 
+class Coffee {
+    constructor(water = 0, milk = 0, coffee_beans = 0, money = 0) {
+        this.water = water;
+        this.milk = milk;
+        this.coffee_beans = coffee_beans;
+        this.money = money;
+    }
+}
+
 class CoffeeMachine {
+
+
     constructor(water, milk, coffee_beans, cups, money) {
         this.water = water;
-        this.milk = milk
-        this.coffee_beans = coffee_beans
-        this.money = money
-        this.cups = cups
+        this.milk = milk;
+        this.coffee_beans = coffee_beans;
+        this.money = money;
+        this.cups = cups;
+        this.espresso = new Coffee(250, 0, 16, 4);
+        this.latte = new Coffee(350, 75, 20, 7);
+        this.cappuccino = new Coffee(200, 100, 12, 6)
+        this.options = {"1": this.espresso, "2": this.latte, "3": this.cappuccino};
     }
 
     take() {
-        console.log(`I gave you ${this.money}`)
-        this.money = 0
+        console.log(`I gave you $${this.money}`);
+        this.money = 0;
     }
 
     fill() {
-        console.log("Write how many ml of water you want to add:")
-        this.water += Number(input())
-        console.log("Write how many ml of milk you want to add:")
-        this.milk += Number(input())
-        console.log("Write how many grams of coffee beans you want to add:")
-        this.coffee_beans += Number(input())
-        console.log("Write how many disposable cups you want to add:")
-        this.cups += Number(input())
+        console.log("Write how many ml of water you want to add:");
+        this.water += Number(input());
+        console.log("Write how many ml of milk you want to add:");
+        this.milk += Number(input());
+        console.log("Write how many grams of coffee beans you want to add:");
+        this.coffee_beans += Number(input());
+        console.log("Write how many disposable cups you want to add:");
+        this.cups += Number(input());
+    }
+
+    check_amount(coffee) {
+        return this.cups > 0 && this.water >= coffee.water &&
+            this.milk >= coffee.milk &&
+            this.coffee_beans >= coffee.coffee_beans;
+    }
+
+    prepare_coffee(coffee) {
+        this.water -= coffee.water;
+        this.milk -= coffee.milk;
+        this.coffee_beans -= coffee.coffee_beans;
+        this.money += coffee.money;
     }
 
     buy() {
-        this.cups -= 1
         console.log("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
-        let option = Number(input())
-        if (option === 1) {
-            this.water -= 250
-            this.coffee_beans -= 16
-            this.money += 4
-        } else if (option === 2) {
-            this.water -= 350
-            this.milk -= 75
-            this.coffee_beans -= 20
-            this.money += 7
-        } else if (option === 3) {
-            this.water -= 200
-            this.milk -= 100
-            this.coffee_beans -= 12
-            this.money += 6
+        let option = input();
+        if (option === "back") {
+            return
+        }
+        let coffee = this.options[option];
+        if (this.check_amount(coffee)) {
+            console.log("I have enough resources, making you a coffee!");
+            this.cups -= 1;
+            this.prepare_coffee(coffee);
+        } else {
+            console.log("Sorry, not enough resources");
         }
     }
 
@@ -58,16 +81,21 @@ $${this.money} of money`)
 
 
 let coffee_machine = new CoffeeMachine(400, 540, 120, 9, 550)
-coffee_machine.info()
 console.log()
-console.log("Write action (buy, fill, take):")
-action = input()
-if (action === "buy") {
-    coffee_machine.buy()
-} else if (action === "take") {
-    coffee_machine.take()
-} else if (action === "fill") {
-    coffee_machine.fill()
+while (true) {
+    console.log("Write action (buy, fill, take, remaining, exit):")
+    let action = input()
+    console.log()
+    if (action === "buy") {
+        coffee_machine.buy()
+    } else if (action === "take") {
+        coffee_machine.take()
+    } else if (action === "fill") {
+        coffee_machine.fill()
+    } else if (action === "remaining") {
+        coffee_machine.info()
+    } else if (action === "exit") {
+        break
+    }
+    console.log()
 }
-console.log()
-coffee_machine.info()
