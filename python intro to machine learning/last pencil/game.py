dc0887f682024d8274741c559b47e6db6ff4fd64
vederko <reshviktor1@gmail.com>
2022-07-22 22:15:50 +0300

    last pencil 4

--- python intro to machine learning/last pencil/game.py
+++ python intro to machine learning/last pencil/game.py
@@ -1,13 +1,40 @@
+import re
+
 if __name__ == "__main__":
-    number_of_pencils = int(input("How many pencils would you like to use:"))
+    while True:
+        number_of_pencils = input("How many pencils would you like to use:")
+        if re.fullmatch(r'\d+', number_of_pencils):
+            if number_of_pencils == '0':
+                print("The number of pencils should be positive")
+            else:
+                number_of_pencils = int(number_of_pencils)
+                break
+        else:
+            print("The number of pencils should be numeric")
     first_player = input("Who will be the first (John, Jack):")
+    while first_player != "John" and first_player != "Jack":
+        first_player = input("Choose between John and Jack")
     second_player = "Jack" if first_player == "John" else "John"
     numeric_flag = 0
     while number_of_pencils > 0:
         print("|" * number_of_pencils)
         current_player = first_player if numeric_flag % 2 == 0 else second_player
         print(f"{current_player}'s turn:")
-        pencils_to_take = int(input())
+        pencils_to_take = input()
+        while pencils_to_take not in ("1", "2", "3") or (re.fullmatch(r'\d+', pencils_to_take) and
+                                                         number_of_pencils < int(pencils_to_take)):
+            if pencils_to_take not in ("1", "2", "3"):
+                print("Possible values: '1', '2' or '3'")
+                pencils_to_take = input()
+                continue
+            if number_of_pencils < int(pencils_to_take):
+                print("Too many pencils were taken")
+                pencils_to_take = input()
+        pencils_to_take = int(pencils_to_take)
         number_of_pencils -= pencils_to_take
         numeric_flag += 1
+        if number_of_pencils == 0:
+            current_player = first_player if numeric_flag % 2 == 0 else second_player
+            print(f"{current_player} won!")
+            break
 
