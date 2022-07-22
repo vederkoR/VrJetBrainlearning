f6c790876df5f394e129b57e7e1eef315f27ccc7
vederko <reshviktor1@gmail.com>
2022-07-22 18:15:37 +0300

    last pencil 2

--- python intro to machine learning/last pencil/game.py
+++ python intro to machine learning/last pencil/game.py
@@ -1,3 +1,6 @@
 if __name__ == "__main__":
-    print('||||||||\nYour turn!')
+    number_of_pencils = int(input("How many pencils would you like to use:"))
+    first_player = input("Who will be the first (John, Jack):")
+    print("|" * number_of_pencils)
+    print(f"{first_player} is going first")
 
