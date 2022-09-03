d3037a30d3741b25009b2dc3a144017ed80a8c76
vederko <reshviktor1@gmail.com>
2022-09-03 20:05:53 +0300

    convoy stage 2

--- python intro to machine learning/convoy shipping company/convoy.py
+++ python intro to machine learning/convoy shipping company/convoy.py
@@ -1,4 +1,3 @@
-# Write your code here
 import pandas as pd
 
 
@@ -15,6 +14,21 @@ class Converter:
             print(f"{number_of_lines} lines were added to {self.file_name}.csv")
         vehicle_series.to_csv(f"{self.file_name}.csv", index=False, header=True)
 
+    def correct(self):
+        vehicle_series = pd.read_csv(f"{self.file_name}.csv")
+        number_of_lines = vehicle_series.shape[0]
+        number_of_rows = vehicle_series.shape[1]
+        corrected_cells = 0
+        for i in range(number_of_lines):
+            for j in range(number_of_rows):
+                if vehicle_series.iat[i, j].isnumeric():
+                    continue
+                else:
+                    corrected_cells += 1
+                    vehicle_series.iat[i, j] = ''.join(char for char in vehicle_series.iat[i, j] if char.isdigit())
+        vehicle_series.to_csv(f"{self.file_name}[CHECKED].csv", index=False, header=True)
+        print(f"{corrected_cells} cells were corrected in {self.file_name}[CHECKED].csv")
+
 
 if __name__ == "__main__":
     print("Input file name")
@@ -22,6 +36,11 @@ if __name__ == "__main__":
         name = input()
         if name.endswith(".xlsx"):
             name = name[0: -5]
+            converter = Converter(name)
+            converter.save_csv()
             break
-    converter = Converter(name)
-    converter.save_csv()
+        if name.endswith(".csv"):
+            name = name[0: -4]
+            converter = Converter(name)
+            break
+    converter.correct()
