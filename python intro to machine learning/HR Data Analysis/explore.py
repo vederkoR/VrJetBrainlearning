d8581a6c4a079ff7f258fb25976704d2272e4259
vederko <reshviktor1@gmail.com>
2022-09-11 15:51:51 +0300

    hr data analysis 1/5' [A

---
+++ python intro to machine learning/HR Data Analysis/explore.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import requests
+import os
+
+
+def files_formation():
+    if not os.path.exists('../Data'):
+        os.mkdir('../Data')
+
+    if ('A_office_data.xml' not in os.listdir('../Data') and
+            'B_office_data.xml' not in os.listdir('../Data') and
+            'hr_data.xml' not in os.listdir('../Data')):
+        print('A_office_data loading.')
+        url = "https://www.dropbox.com/s/jpeknyzx57c4jb2/A_office_data.xml?dl=1"
+        r = requests.get(url, allow_redirects=True)
+        open('../Data/A_office_data.xml', 'wb').write(r.content)
+        print('Loaded.')
+
+        print('B_office_data loading.')
+        url = "https://www.dropbox.com/s/hea0tbhir64u9t5/B_office_data.xml?dl=1"
+        r = requests.get(url, allow_redirects=True)
+        open('../Data/B_office_data.xml', 'wb').write(r.content)
+        print('Loaded.')
+
+        print('hr_data loading.')
+        url = "https://www.dropbox.com/s/u6jzqqg1byajy0s/hr_data.xml?dl=1"
+        r = requests.get(url, allow_redirects=True)
+        open('../Data/hr_data.xml', 'wb').write(r.content)
+        print('Loaded.')
+
+
+def re_indexer(pd_df, series):
+    re_indexed_list = [series + str(i) for i in list(pd_df['employee_office_id'].values)]
+    pd_df.index = re_indexed_list
+
+
+if __name__ == '__main__':
+    files_formation()
+    pd_a = pd.read_xml("../Data/A_office_data.xml")
+    pd_b = pd.read_xml("../Data/B_office_data.xml")
+    pd_hr = pd.read_xml("../Data/hr_data.xml")
+    re_indexer(pd_a, "A")
+    re_indexer(pd_b, "B")
+    pd_hr = pd_hr.set_index('employee_id')
+
+    print(list(pd_a.index))
+    print(list(pd_b.index))
+    print(list(pd_hr.index))
