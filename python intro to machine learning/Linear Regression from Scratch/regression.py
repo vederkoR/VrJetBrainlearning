f86f936cae76447042986bf73555443ed95aa950
Viktor Reshetnikov <reshviktor1@gmail.com>
2022-09-24 13:36:44 +0300

    linear regretion stage 2/4

--- python intro to machine learning/Linear Regression from Scratch/regression.py
+++ python intro to machine learning/Linear Regression from Scratch/regression.py
@@ -5,17 +5,26 @@ class CustomLinearRegression:
 
     def __init__(self, *, fit_intercept=True):
         self.fit_intercept = fit_intercept
-        self.coefficient = None
-        self.intercept = None
+        self.coefficient = np.array([])
+        self.intercept = float()
+        self.coefficients = None
 
     def fit(self, X, y):
-        X = np.matrix([[1, i] for i in X])
-        self.coefficient = np.dot(np.dot(np.linalg.inv(np.dot(X.T, X)), X.T), y)
-        final_dict = {'Intercept': np.array(self.coefficient.tolist()[0][0]),
-                      'Coefficient': np.array(self.coefficient.tolist()[0][1])}
-        print(final_dict)
+        if self.fit_intercept:
+            X = np.matrix([[1, i] for i in X])
+            self.coefficients = np.dot(np.dot(np.linalg.inv(np.dot(X.T, X)), X.T), y).tolist()[0]
+            self.coefficient = self.coefficients.tolist()[0]
+            self.intercept = self.coefficients.tolist()[0]
+        else:
+            self.coefficients = np.dot(np.dot(np.linalg.inv(np.dot(X.T, X)), X.T), y).tolist()[0]
+
+    def predict(self, X):
+        return (X @ np.array(self.coefficients)).tolist()[0]
 
 
-b = CustomLinearRegression()
-b.fit(X=np.array([4.0, 4.5, 5, 5.5, 6.0, 6.5, 7.0]),
+d = CustomLinearRegression(fit_intercept=False)
+d.fit(X=np.matrix([[4, 1, 11], [4.5, -3, 15], [5, 2, 12], [5.5, 5, 9], [6, 0, 18], [6.5, 3, 13], [7, 6, 16]]),
       y=np.array([33, 42, 45, 51, 53, 61, 62]))
+
+print(np.array(
+    d.predict(X=np.matrix([[4, 1, 11], [4.5, -3, 15], [5, 2, 12], [5.5, 5, 9], [6, 0, 18], [6.5, 3, 13], [7, 6, 16]]))))
