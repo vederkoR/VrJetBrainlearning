780b5c033370ad6ceafaea7ebcc2db62b25da886
Viktor Reshetnikov <reshviktor1@gmail.com>
2022-10-11 14:21:32 +0300

    Properties of Compact Group Gals stage 3

--- python intro to machine learning/Properties of Compact Groups of Galaxies/task/main.py
+++ python intro to machine learning/Properties of Compact Groups of Galaxies/task/main.py
@@ -1,13 +1,40 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from scipy import stats
 
-df = pd.read_csv("data/groups.tsv", delimiter='\t')
-# print(df.dropna()[df.features == 1].mean_mu.mean(), df.dropna()[df.features == 0].mean_mu.mean())
-sample_1 = df.dropna()[df.features == 1].mean_mu
-sample_2 = df.dropna()[df.features == 0].mean_mu
+# df = pd.read_csv("data/groups.tsv", delimiter='\t')
+# # print(df.dropna()[df.features == 1].mean_mu.mean(), df.dropna()[df.features == 0].mean_mu.mean())
+# sample_1 = df.dropna()[df.features == 1].mean_mu
+# sample_2 = df.dropna()[df.features == 0].mean_mu
+#
+# p1 = stats.shapiro(sample_1).pvalue
+# p2 = stats.shapiro(sample_2).pvalue
+# stat, p = stats.fligner(sample_1, sample_2)
+# anova_p = stats.f_oneway(sample_1, sample_2).pvalue
+# print(p1, p2, p, anova_p)
 
-p1 = stats.shapiro(sample_1).pvalue
-p2 = stats.shapiro(sample_2).pvalue
-stat, p = stats.fligner(sample_1, sample_2)
-anova_p = stats.f_oneway(sample_1, sample_2).pvalue
-print(p1, p2, p, anova_p)
+isolated_gals = pd.read_csv("data/isolated_galaxies.tsv", delimiter='\t')
+gal_morph = pd.read_csv("data/galaxies_morphology.tsv", delimiter='\t')
+
+# print(isolated_gals.head(5))
+# print(gal_morph.head(5))
+# plt.hist([isolated_gals.n, gal_morph.n],
+#          stacked=True,
+#          label=["isolated galaxies", "groups galaxies"],
+#          color=['red', '#00958B'],  # data color
+#          edgecolor="black",  # edge color
+#          lw=0.5,  # width of edge color
+#          rwidth=0.8,  # set bar width (with bargaps),
+#          alpha=0.7  # set transparent color
+#          )
+#
+#
+# plt.ylabel("Count")
+# plt.xlabel("n")
+# plt.legend()
+# plt.show()
+
+frac_1 = len(gal_morph[gal_morph.n > 2].n) / len(gal_morph.n)
+frac_2 = len(isolated_gals[isolated_gals.n > 2].n) / len(isolated_gals.n)
+p = stats.ks_2samp(isolated_gals.n, gal_morph.n).pvalue
+print(frac_1, frac_2, p)
--- python intro to machine learning/Properties of Compact Groups of Galaxies/task/tests.py
+++ python intro to machine learning/Properties of Compact Groups of Galaxies/task/tests.py
@@ -1,13 +1,5 @@
 from hstest import *
 from math import isclose
-# import re
-
-
-# def is_float(element):
-#     if re.match(r'([+-]?(?:[0-9]*[.])?[0-9]+)', element) is None:
-#         return False
-#     else:
-#         return True
 
 
 def is_float(num: str):
@@ -18,7 +10,7 @@ def is_float(num: str):
         return False
 
 
-class Stage1Test(PlottingTest):
+class Stage3Test(PlottingTest):
     def check_outputs_number(self, values_number: int, user_output: str):
         outputs = user_output.split()
 
@@ -26,7 +18,7 @@ class Stage1Test(PlottingTest):
             raise WrongAnswer(f"Answer '{user_output}' contains non-numeric values.")
 
         if len(outputs) != values_number:
-            raise WrongAnswer(f"Answer contains {len(outputs)} values, but {values_number} values are expexted.")
+            raise WrongAnswer(f"Answer contains {len(outputs)} values, but {values_number} values are expected.")
 
     def check_num_values(self, values: list, user_values: list, message: str, rel_tol=1.0e-3):
         if not all(isclose(value, user_value, rel_tol=rel_tol) for value, user_value in zip(values, user_values)):
@@ -41,25 +33,33 @@ class Stage1Test(PlottingTest):
             raise WrongAnswer("Seems like your program does not show any output.")
 
         # check output format
-        self.check_outputs_number(2, user_output)
+        self.check_outputs_number(3, user_output)
 
-        # check values
-        user_values = [float(value) for value in user_output.split()]
-
-        first_value = [26.678053926691177]
-        self.check_num_values(first_value, user_values[:1],
-                              "The first submitted value (mean of mean surface brightness of the IGL for a groups with features) is wrong.\n"
+        # check fraction of galaxies with n > 2 for CGs galaxies
+        cgs_fraction = [0.6394557823129252]
+        user_values = [float(value) for value in user_output.split()][:1]
+        self.check_num_values(cgs_fraction, user_values,
+                              "The fraction of galaxies with Sersic index n > 2 for groups galaxies is wrong.\n"
                               "Make sure that you provide numbers in the correct order.",
                               rel_tol=1.0e-2)
 
-        first_value = [27.038330296424938]
-        self.check_num_values(first_value, user_values[1:2],
-                              "The second submitted value (mean of mean surface brightness of the IGL for a groups without features) is wrong.\n"
+        # check fraction of galaxies with n > 2 for isolated galaxies
+        p_values = [0.29245283018867924]
+        user_values = [float(value) for value in user_output.split()][1:2]
+        self.check_num_values(p_values, user_values,
+                              "The fraction of galaxies with Sersic index n > 2 for isolated galaxies is wrong.\n"
                               "Make sure that you provide numbers in the correct order.",
                               rel_tol=1.0e-2)
+
+        # check Kolmogorov-Smirnov test p-value
+        p_values = [4.163336342344337e-14]
+        user_values = [float(value) for value in user_output.split()][2:3]
+        self.check_num_values(p_values, user_values, "The p-value of Kolmogorov-Smirnov test is wrong.\n"
+                              "Make sure that you provide numbers in the correct order.",
+                              rel_tol=5.0e-2)
 
         return CheckResult.correct()
 
 
 if __name__ == '__main__':
-    Stage1Test().run_tests()
+    Stage3Test().run_tests()
