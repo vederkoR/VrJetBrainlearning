f6468abe14bd81135ca3464ad5ab4bde60b62ac3
Viktor Reshetnikov <reshviktor1@gmail.com>
2022-10-20 14:44:05 +0300

    handwritten digit recognition mac learning stage 2/5

--- python intro to machine learning/Classification of Handwritten Digits/analysis.py
+++ python intro to machine learning/Classification of Handwritten Digits/analysis.py
@@ -1,13 +1,27 @@
 # write your code here
+from collections import Counter
 import tensorflow as tf
 import numpy as np
+from sklearn.model_selection import train_test_split
+import pandas as pd
 
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data(path="mnist.npz")
+(x_train, y_train), (_, _) = tf.keras.datasets.mnist.load_data(path="mnist.npz")
 
-classes = np.array(y_train)
+# # Stage 1
+# classes = np.array(y_train)
+# x_train = x_train.reshape(len(x_train), -1)
+# print("Classes:", np.unique(classes))
+# print("Features' shape:", x_train.shape)
+# print("Target's shape:", y_train.shape)
+# print(f"min: {np.min(x_train):.1f}, max: {np.max(x_train):.1f}")
+
+# # Stage 2
 x_train = x_train.reshape(len(x_train), -1)
-print("Classes:", np.unique(classes))
-print("Features' shape:", x_train.shape)
-print("Target's shape:", y_train.shape)
-print(f"min: {np.min(x_train):.1f}, max: {np.max(x_train):.1f}")
+X_train, X_test, y_train, y_test = train_test_split(x_train[0:6000], y_train[0:6000], train_size=0.7, random_state=40)
+print("x_train shape:", X_train.shape)
+print("x_test shape:", X_test.shape)
+print("y_train shape:", y_train.shape)
+print("y_test shape:", y_test.shape)
+print("Proportion of samples per class in train set:")
+print(pd.Series(y_train).value_counts(normalize=True))
 
