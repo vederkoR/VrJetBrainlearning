374fd3adf90af4ceec0997e0d08a7e20e5735b2a
Viktor Reshetnikov <reshviktor1@gmail.com>
2022-10-31 23:20:27 +0200

    Dominoes 3/5

--- python intro to machine learning/Dominoes/task/dominoes/dominoes.py
+++ python intro to machine learning/Dominoes/task/dominoes/dominoes.py
@@ -1,3 +1,5 @@
+import itertools
+import re
 from itertools import combinations_with_replacement
 from random import shuffle
 
@@ -16,6 +18,7 @@ class Dominoes:
         self.stock = None
         self.current_player = None
         self.snake = list()
+        self.current_state = ""
 
     def shuffle_species(self):
         shuffle(self.species)
@@ -47,30 +50,84 @@ class Dominoes:
         self.distribute_species()
         self.initiate_current_player()
 
+    def snack_representation(self):
+        snake_str = ""
+        if len(self.snake) <= 6:
+            for i in range(len(self.snake)):
+                snake_str += f'{[[int(i), int(j)] for (i, j) in self.snake][i]}'
+        else:
+            for i in range(3):
+                snake_str += f'{[[int(i), int(j)] for (i, j) in self.snake][i]}'
+            snake_str += "..."
+            for i in range(len(self.snake) - 3, len(self.snake)):
+                snake_str += f'{[[int(i), int(j)] for (i, j) in self.snake][i]}'
+        return snake_str
+
+    def graph_representation(self):
+        print('======================================================================')
+        print(f"Stock size: {len(self.stock)}")
+        print(f"Computer pieces: {len(self.player_2.domino_pieces)}")
+        print()
+        print(self.snack_representation())
+        print()
+        print("Your pieces:")
+        for index, value in enumerate(self.player_1.domino_pieces):
+            print(f"{index + 1}:{[int(value[0]), int(value[1])]}")
+        print()
+
+    def turn(self):
+        if self.current_player == self.player_1:
+            while True:
+                piece_to_take = input()
+                template = r"-?" + "[" + "".join(
+                    [str(i) for i in range(0, len(self.current_player.domino_pieces) + 1)]) + "]"
+                checker = re.fullmatch(template, piece_to_take)
+                if checker:
+                    break
+                else:
+                    print("Invalid input. Please try again.")
+            if piece_to_take == '0':
+                self.player_1.domino_pieces.append(self.stock.pop())
+                return
+        else:
+            pool = [str(i) for i in range(1, len(self.current_player.domino_pieces) + 1)]
+            shuffle(pool)
+            piece_to_take = pool[0]
+        if piece_to_take.startswith("-"):
+            self.snake.insert(0, self.current_player.domino_pieces.pop(int(piece_to_take[-1]) - 1))
+        else:
+            self.snake.insert(len(self.snake), self.current_player.domino_pieces.pop(int(piece_to_take[-1]) - 1))
+
+    def status_check(self):
+        if not self.player_1.domino_pieces:
+            self.current_state = "Status: The game is over. You won!"
+            return True
+        elif not self.player_2.domino_pieces:
+            self.current_state = "Status: The game is over. The computer won!"
+            return True
+        elif self.snake[0][0] == self.snake[-1][-1] and list(itertools.chain(*self.snake)).count(self.snake[0][0]) >= 8:
+            self.current_state = "Status: The game is over. It's a draw!"
+            return True
+        elif self.current_player.name == 'computer':
+            self.current_state = "Status: Computer is about to make a move. Press Enter to continue..."
+            return False
+        else:
+            self.current_state = "Status: It's your turn to make a move. Enter your command."
+            return False
+
 
 if __name__ == "__main__":
     game = Dominoes()
     game.start()
-    print('======================================================================')
-    print(f"Stock size: {len(game.stock)}")
-    print(f"Computer pieces: {len(game.player_2.domino_pieces)}")
-    print()
-    print(f'{[[int(i), int(j)] for (i, j) in game.snake][0]}')
-    print()
-    print("Your pieces:")
-    for index, value in enumerate(game.player_1.domino_pieces):
-        print(f"{index + 1}:{[int(value[0]), int(value[1])]}")
-    print()
-    if game.current_player.name == 'computer':
-        print("Status: Computer is about to make a move. Press Enter to continue...")
-    else:
-        print("Status: It's your turn to make a move. Enter your command.")
+    while True:
 
-    # # stage 1
-    # game = Dominoes()
-    # game.start()
-    # print(f"Stock pieces: {[[int(i), int(j)] for (i, j) in game.stock]}")
-    # print(f"Computer pieces: {[[int(i), int(j)] for (i, j) in game.player_2.domino_pieces]}")
-    # print(f"Player pieces: {[[int(i), int(j)] for (i, j) in game.player_1.domino_pieces]}")
-    # print(f"Domino snake: {[[int(i), int(j)] for (i, j) in game.snake]}")
-    # print(f"Status: {game.current_player.name}")
+        game.graph_representation()
+        a = game.status_check()
+        print(game.current_state)
+        if a:
+            break
+        game.turn()
+        if game.current_player.name == "computer":
+            _ = input()
+
+        game.change_player()
