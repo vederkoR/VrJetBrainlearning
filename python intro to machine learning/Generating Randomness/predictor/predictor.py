6a3563eab94bb8d9f808f16fb5ccc158c79a8a9f
Viktor Reshetnikov <reshviktor1@gmail.com>
2022-11-01 15:30:51 +0200

    Generating Randomness 2/4

--- python intro to machine learning/Generating Randomness/predictor/predictor.py
+++ python intro to machine learning/Generating Randomness/predictor/predictor.py
@@ -1,3 +1,6 @@
+import itertools
+from collections import Counter
+
 MINIMAL_LENGTH = 100
 string_data = ""
 
@@ -9,19 +12,32 @@ def digits_request():
     string_data += processed_digits
 
 
-def interface():
+def interface_1():
     global string_data
     while True:
         print("Print a random string containing 0 or 1:\n")
         digits_request()
         if len(string_data) >= MINIMAL_LENGTH:
-            print(f"\nFinal data string:\n{string_data}")
+            print(f"\nFinal data string:\n{string_data}\n")
             break
         print(f"Current data length is {len(string_data)}, {MINIMAL_LENGTH - len(string_data)} symbols left")
 
 
+def find_occurrences():
+    list_all_triads = [string_data[i:i + 3] + " → " + string_data[i + 3] for i in range(len(string_data) - 3)]
+    return Counter(list_all_triads)
+
+
+def interface_2():
+    frequencies = find_occurrences()
+    for i in map(''.join, itertools.product('01', repeat=3)):
+        print(i + ": " + f"{frequencies[i + ' → 0']},{frequencies[i + ' → 1']}")
+
+
 def main():
-    interface()
+    interface_1()
+    find_occurrences()
+    interface_2()
 
 
 if __name__ == "__main__":
