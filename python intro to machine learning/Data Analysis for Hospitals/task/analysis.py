f5e36f8c7cd3da363a685b4259978fbc5dcbb515
Viktor Reshetnikov <reshviktor1@gmail.com>
2022-11-08 18:22:13 +0200

    data analysis for hospitals 3/5

--- python intro to machine learning/Data Analysis for Hospitals/task/analysis.py
+++ python intro to machine learning/Data Analysis for Hospitals/task/analysis.py
@@ -14,4 +14,21 @@ sport_hospitals = sport_hospitals.rename(columns={'Hospital': 'hospital', 'Male/
 
 all_hospitals = pd.concat([general_hospitals, prenatal_hospitals, sport_hospitals], ignore_index=True)
 all_hospitals = all_hospitals.drop(columns=['Unnamed: 0'])
+all_hospitals = all_hospitals.dropna(axis=0, how='all')
+
+all_hospitals['gender'] = all_hospitals['gender'].replace(['man', 'male'], "m")
+all_hospitals['gender'] = all_hospitals['gender'].replace(['woman', 'female'], "f")
+all_hospitals['gender'] = all_hospitals['gender'].fillna("f")
+
+all_hospitals['bmi'] = all_hospitals['bmi'].fillna(0)
+all_hospitals['diagnosis'] = all_hospitals['diagnosis'].fillna(0)
+all_hospitals['blood_test'] = all_hospitals['blood_test'].fillna(0)
+all_hospitals['ecg'] = all_hospitals['ecg'].fillna(0)
+all_hospitals['ultrasound'] = all_hospitals['ultrasound'].fillna(0)
+all_hospitals['mri'] = all_hospitals['mri'].fillna(0)
+all_hospitals['xray'] = all_hospitals['xray'].fillna(0)
+all_hospitals['children'] = all_hospitals['children'].fillna(0)
+all_hospitals['months'] = all_hospitals['months'].fillna(0)
+
+print(f"Data shape: {all_hospitals.shape}")
 print(all_hospitals.sample(n=20, random_state=30))
