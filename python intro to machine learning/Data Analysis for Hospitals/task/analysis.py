67b54676f9e78094db3cb269b8bb3bf567cbc41c
Viktor Reshetnikov <reshviktor1@gmail.com>
2022-11-08 14:16:57 +0200

    data analysis for hospitals 2/5

--- python intro to machine learning/Data Analysis for Hospitals/task/analysis.py
+++ python intro to machine learning/Data Analysis for Hospitals/task/analysis.py
@@ -9,6 +9,9 @@ prenatal_hospitals = pd.read_csv(
 sport_hospitals = pd.read_csv(
     r"C:\Users\reshv\PycharmProjects\Data Analysis for Hospitals\Data Analysis for Hospitals\task\test\sports.csv")
 
-print(general_hospitals.head(20))
-print(prenatal_hospitals.head(20))
-print(sport_hospitals.head(20))
+prenatal_hospitals = prenatal_hospitals.rename(columns={'HOSPITAL': 'hospital', 'Sex': 'gender'})
+sport_hospitals = sport_hospitals.rename(columns={'Hospital': 'hospital', 'Male/female': 'gender'})
+
+all_hospitals = pd.concat([general_hospitals, prenatal_hospitals, sport_hospitals], ignore_index=True)
+all_hospitals = all_hospitals.drop(columns=['Unnamed: 0'])
+print(all_hospitals.sample(n=20, random_state=30))
