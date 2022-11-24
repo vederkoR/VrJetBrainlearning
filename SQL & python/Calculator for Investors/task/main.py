5a5062f9de97cc1f8f2e09c0360e93f2f3533cb0
Viktor Reshetnikov <reshviktor1@gmail.com>
2022-11-24 21:41:15 +0200

    calculator for investors stage 3/4

--- SQL & python/Calculator for Investors/task/main.py
+++ SQL & python/Calculator for Investors/task/main.py
@@ -1,4 +1,18 @@
+import os
+
+import pandas as pd
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+
 class MenuDisplay:
+    @staticmethod
+    def display_welcome():
+        print("Welcome to the Investor Program!\n")
+
     @staticmethod
     def display_main():
         print("MAIN MENU\n0 Exit\n1 CRUD operations\n2 Show top ten companies by criteria\n")
@@ -29,7 +43,209 @@ class MenuDisplay:
         print("Enter an option:")
 
 
+class Companies(Base):
+    __tablename__ = 'companies'
+    ticker = Column(String(50), primary_key=True)
+    name = Column(String(50))
+    sector = Column(String(50))
+
+
+class Financial(Base):
+    __tablename__ = 'financial'
+    ticker = Column(String(50), primary_key=True)
+    ebitda = Column(Integer)
+    sales = Column(Integer)
+    net_profit = Column(Integer)
+    market_price = Column(Integer)
+    net_debt = Column(Integer)
+    assets = Column(Integer)
+    equity = Column(Integer)
+    cash_equivalents = Column(Integer)
+    liabilities = Column(Integer)
+
+
+def div_none(a, b):
+    if a is None or b is None:
+        return None
+    else:
+        return f'{a / b:.2f}'
+
+
+def request_fields_com():
+    print("Enter ticker")
+    ticker = input()
+    print("Enter company")
+    company = input()
+    print("Enter industries")
+    industries = input()
+    return ticker, company, industries
+
+
+def request_fields_fin():
+    print("Enter ebitda")
+    ebitda = input()
+    print("Enter sales")
+    sales = input()
+    print("Enter net profit")
+    net_profit = input()
+    print("Enter market price")
+    market_price = input()
+    print("Enter net debt")
+    net_debt = input()
+    print("Enter assets")
+    assets = input()
+    print("Enter equity")
+    equity = input()
+    print("Enter cash equivalents")
+    cash_equivalents = input()
+    print("Enter liabilities")
+    liabilities = input()
+    return ebitda, sales, net_profit, market_price, net_debt, assets, equity, cash_equivalents, liabilities
+
+
+def creat_company(session):
+    (ticker, company, industries) = request_fields_com()
+    (ebitda, sales, net_profit, market_price, net_debt, assets, equity,
+     cash_equivalents, liabilities) = request_fields_fin()
+
+    session.add(Companies(ticker=ticker,
+                          name=company,
+                          sector=industries))
+
+    session.add(Financial(ticker=ticker,
+                          ebitda=int(ebitda),
+                          sales=int(sales),
+                          net_profit=int(net_profit),
+                          market_price=int(market_price),
+                          net_debt=int(net_debt),
+                          assets=int(assets),
+                          equity=int(equity),
+                          cash_equivalents=int(cash_equivalents),
+                          liabilities=int(liabilities)))
+    session.commit()
+    print('Company created successfully!\n')
+
+
+def search_company(session):
+    query_companies = session.query(Companies.name, Companies.ticker)
+    print('Enter company name:')
+    name_fragment = input()
+    filtered = [i for i in query_companies.filter(Companies.name.ilike('%' + name_fragment + '%'))]
+    if not filtered:
+        print("Company not found!\n")
+        return
+    for inx, entry in enumerate(filtered):
+        print(f"{inx} {entry[0]}")
+    print("Enter company number:")
+    which_compony = int(input())
+    return filtered[which_compony]
+
+
+def read_company(session):
+    company = search_company(session)
+    if not company:
+        return
+    print(f"{company[1]} {company[0]}")
+    ticker = company[1]
+    query_financial = session.query(Financial.ticker,
+                                    Financial.market_price,
+                                    Financial.net_profit,
+                                    Financial.sales,
+                                    Financial.assets,
+                                    Financial.net_debt,
+                                    Financial.ebitda,
+                                    Financial.equity,
+                                    Financial.liabilities)
+
+    values = list(*query_financial.filter(Financial.ticker == ticker))
+    print(f"P/E = {div_none(values[1], values[2])}")
+    print(f"P/S = {div_none(values[1], values[3])}")
+    print(f"P/B = {div_none(values[1], values[4])}")
+    print(f"ND/EBITDA = {div_none(values[5], values[6])}")
+    print(f"ROE = {div_none(values[2], values[7])}")
+    print(f"ROA = {div_none(values[2], values[4])}")
+    print(f"L/A = {div_none(values[8], values[4])}")
+    print('\n')
+
+
+def update_company(session):
+    company = search_company(session)
+    if not company:
+        return
+    ticker_for_search = company[1]
+    (ebitda, sales, net_profit, market_price, net_debt, assets, equity,
+     cash_equivalents, liabilities) = request_fields_fin()
+    update_query_fin = session.query(Financial).filter(Financial.ticker == ticker_for_search)
+    update_query_fin.update({"ebitda": int(ebitda),
+                             "sales": int(sales),
+                             "net_profit": int(net_profit),
+                             "market_price": int(market_price),
+                             "net_debt": int(net_debt),
+                             "assets": int(assets),
+                             "equity": int(equity),
+                             "cash_equivalents": int(cash_equivalents),
+                             "liabilities": int(liabilities)})
+    print("Company updated successfully!\n")
+    session.commit()
+
+
+def delete_company(session):
+    company = search_company(session)
+    if not company:
+        return
+    ticker_for_search = company[1]
+    update_query_fin = session.query(Financial).filter(Financial.ticker == ticker_for_search)
+    update_query_fin.delete()
+    update_query_com = session.query(Companies).filter(Companies.ticker == ticker_for_search)
+    update_query_com.delete()
+    print("Company deleted successfully!\n")
+    session.commit()
+
+
+def list_companies(session):
+    print("COMPANY LIST")
+    query_companies = session.query(Companies.ticker, Companies.name, Companies.sector).order_by(Companies.ticker)
+    for row in query_companies:
+        print(row.ticker, row.name, row.sector)
+    print()
+
+
+def db_setup(is_db_exist):
+    engine = create_engine('sqlite:///investor.db', echo=False)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    if not is_db_exist:
+        companies_data = pd.read_csv("./miscellaneous/companies.csv")
+        financial_data = pd.read_csv('./miscellaneous/financial.csv')
+
+        for index, row in companies_data.iterrows():
+            session.add(Companies(ticker=row['ticker'],
+                                  name=row['name'],
+                                  sector=row['sector']))
+
+        for index, row in financial_data.iterrows():
+            session.add(Financial(ticker=row['ticker'],
+                                  ebitda=row['ebitda'],
+                                  sales=row['sales'],
+                                  net_profit=row['net_profit'],
+                                  market_price=row['market_price'],
+                                  net_debt=row['net_debt'],
+                                  assets=row['assets'],
+                                  equity=row['equity'],
+                                  cash_equivalents=row['cash_equivalents'],
+                                  liabilities=row['liabilities']))
+
+    session.commit()
+    return session
+
+
 def main():
+    main_session = db_setup(os.path.exists('investor.db'))
+    MenuDisplay.display_welcome()
+
     while True:
         MenuDisplay.display_main()
         MenuDisplay.display_request()
@@ -43,8 +259,16 @@ def main():
                 match input():
                     case '0':
                         continue
-                    case '1' | '2' | '3' | '4' | '5':
-                        MenuDisplay.display_not_implemented()
+                    case '1':
+                        creat_company(main_session)
+                    case '2':
+                        read_company(main_session)
+                    case '3':
+                        update_company(main_session)
+                    case '4':
+                        delete_company(main_session)
+                    case '5':
+                        list_companies(main_session)
                     case _:
                         MenuDisplay.display_error()
 
@@ -64,3 +288,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
