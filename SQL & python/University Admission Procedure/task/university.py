e5e88135313fe64e96bce1063ec83e1c2292132f
Viktor Reshetnikov <reshviktor1@gmail.com>
2022-12-04 00:52:33 +0200

    University Admission Procedure stage 7/7 finish

--- SQL & python/University Admission Procedure/task/university.py
+++ SQL & python/University Admission Procedure/task/university.py
@@ -19,40 +19,40 @@ def main():
     with open('applicants.txt', mode='r') as file:
         all_students = [student.strip().split(" ") for student in file.readlines()]
 
-        bio_students_rnd_1 = sorted([i for i in all_students if i[6] == "Biotech"],
-                                    key=lambda x: (-int(x[2]) - int(x[3]), x[0] + x[1]))
-        bio_students_rnd_2 = sorted([i for i in all_students if i[7] == "Biotech"],
-                                    key=lambda x: (-int(x[2]) - int(x[3]), x[0] + x[1]))
-        bio_students_rnd_3 = sorted([i for i in all_students if i[8] == "Biotech"],
-                                    key=lambda x: (-int(x[2]) - int(x[3]), x[0] + x[1]))
+        bio_students_rnd_1 = sorted([i for i in all_students if i[7] == "Biotech"],
+                                    key=lambda x: (min(((-int(x[2]) - int(x[3])) / 2), -int(x[6])), x[0] + x[1]))
+        bio_students_rnd_2 = sorted([i for i in all_students if i[8] == "Biotech"],
+                                    key=lambda x: (min(((-int(x[2]) - int(x[3])) / 2), -int(x[6])), x[0] + x[1]))
+        bio_students_rnd_3 = sorted([i for i in all_students if i[9] == "Biotech"],
+                                    key=lambda x: (min(((-int(x[2]) - int(x[3])) / 2), -int(x[6])), x[0] + x[1]))
 
-        che_students_rnd_1 = sorted([i for i in all_students if i[6] == "Chemistry"],
-                                    key=lambda x: (-int(x[3]), x[0] + x[1]))
-        che_students_rnd_2 = sorted([i for i in all_students if i[7] == "Chemistry"],
-                                    key=lambda x: (-int(x[3]), x[0] + x[1]))
-        che_students_rnd_3 = sorted([i for i in all_students if i[8] == "Chemistry"],
-                                    key=lambda x: (-int(x[3]), x[0] + x[1]))
+        che_students_rnd_1 = sorted([i for i in all_students if i[7] == "Chemistry"],
+                                    key=lambda x: (min(-int(x[3]), -int(x[6])), x[0] + x[1]))
+        che_students_rnd_2 = sorted([i for i in all_students if i[8] == "Chemistry"],
+                                    key=lambda x: (min(-int(x[3]), -int(x[6])), x[0] + x[1]))
+        che_students_rnd_3 = sorted([i for i in all_students if i[9] == "Chemistry"],
+                                    key=lambda x: (min(-int(x[3]), -int(x[6])), x[0] + x[1]))
 
-        eng_students_rnd_1 = sorted([i for i in all_students if i[6] == "Engineering"],
-                                    key=lambda x: (-int(x[4]) - int(x[5]), x[0] + x[1]))
-        eng_students_rnd_2 = sorted([i for i in all_students if i[7] == "Engineering"],
-                                    key=lambda x: (-int(x[4]) - int(x[5]), x[0] + x[1]))
-        eng_students_rnd_3 = sorted([i for i in all_students if i[8] == "Engineering"],
-                                    key=lambda x: (-int(x[4]) - int(x[5]), x[0] + x[1]))
+        eng_students_rnd_1 = sorted([i for i in all_students if i[7] == "Engineering"],
+                                    key=lambda x: (min(((-int(x[4]) - int(x[5])) / 2), -int(x[6])), x[0] + x[1]))
+        eng_students_rnd_2 = sorted([i for i in all_students if i[8] == "Engineering"],
+                                    key=lambda x: (min(((-int(x[4]) - int(x[5])) / 2), -int(x[6])), x[0] + x[1]))
+        eng_students_rnd_3 = sorted([i for i in all_students if i[9] == "Engineering"],
+                                    key=lambda x: (min(((-int(x[4]) - int(x[5])) / 2), -int(x[6])), x[0] + x[1]))
 
-        math_students_rnd_1 = sorted([i for i in all_students if i[6] == "Mathematics"],
-                                     key=lambda x: (-int(x[4]), x[0] + x[1]))
-        math_students_rnd_2 = sorted([i for i in all_students if i[7] == "Mathematics"],
-                                     key=lambda x: (-int(x[4]), x[0] + x[1]))
-        math_students_rnd_3 = sorted([i for i in all_students if i[8] == "Mathematics"],
-                                     key=lambda x: (-int(x[4]), x[0] + x[1]))
+        math_students_rnd_1 = sorted([i for i in all_students if i[7] == "Mathematics"],
+                                     key=lambda x: (min(-int(x[4]), -int(x[6])), x[0] + x[1]))
+        math_students_rnd_2 = sorted([i for i in all_students if i[8] == "Mathematics"],
+                                     key=lambda x: (min(-int(x[4]), -int(x[6])), x[0] + x[1]))
+        math_students_rnd_3 = sorted([i for i in all_students if i[9] == "Mathematics"],
+                                     key=lambda x: (min(-int(x[4]), -int(x[6])), x[0] + x[1]))
 
-        phis_students_rnd_1 = sorted([i for i in all_students if i[6] == "Physics"],
-                                     key=lambda x: (-int(x[4]) - int(x[2]), x[0] + x[1]))
-        phis_students_rnd_2 = sorted([i for i in all_students if i[7] == "Physics"],
-                                     key=lambda x: (-int(x[4]) - int(x[2]), x[0] + x[1]))
-        phis_students_rnd_3 = sorted([i for i in all_students if i[8] == "Physics"],
-                                     key=lambda x: (-int(x[4]) - int(x[2]), x[0] + x[1]))
+        phis_students_rnd_1 = sorted([i for i in all_students if i[7] == "Physics"],
+                                     key=lambda x: (min(((-int(x[4]) - int(x[2])) / 2), -int(x[6])), x[0] + x[1]))
+        phis_students_rnd_2 = sorted([i for i in all_students if i[8] == "Physics"],
+                                     key=lambda x: (min(((-int(x[4]) - int(x[2])) / 2), -int(x[6])), x[0] + x[1]))
+        phis_students_rnd_3 = sorted([i for i in all_students if i[9] == "Physics"],
+                                     key=lambda x: (min(((-int(x[4]) - int(x[2])) / 2), -int(x[6])), x[0] + x[1]))
 
     students_dist = [bio_students_rnd_1, che_students_rnd_1, eng_students_rnd_1,
                      math_students_rnd_1, phis_students_rnd_1,
@@ -60,7 +60,6 @@ def main():
                      math_students_rnd_2, phis_students_rnd_2,
                      bio_students_rnd_3, che_students_rnd_3, eng_students_rnd_3,
                      math_students_rnd_3, phis_students_rnd_3]
-    print(len(students_dist))
     accepted_students = []
     for inx, eval_round in enumerate(students_dist):
         department = deps[inx % 5]
@@ -74,9 +73,11 @@ def main():
 
     for dep in deps:
         term = dep[2:]
-        for student in sorted(term, key=lambda x: (-sum([int(x[i]) for i in dep[1]]), x[0] + x[1])):
+        for student in sorted(term, key=lambda x: (min(-sum([int(x[i]) for i in dep[1]]) / len(dep[1]),
+                                                       -int(x[6])), x[0] + x[1])):
             with open(dep[0] + '.txt', mode='a+') as file:
-                file.write(f"{student[0]} {student[1]} {sum([float(student[i]) for i in dep[1]]) / len(dep[1])}\n")
+                file.write(
+                    f"{student[0]} {student[1]} {max(sum([float(student[i]) for i in dep[1]]) / len(dep[1]), float(int(student[6])))}\n")
 
 
 if __name__ == '__main__':
