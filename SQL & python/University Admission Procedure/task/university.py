9d9ffe7a97d44e6e5752b8d23fdb4800873cf486
Viktor Reshetnikov <reshviktor1@gmail.com>
2022-12-03 14:37:37 +0200

    University Admission Procedure stage 1/7

---
+++ SQL & python/University Admission Procedure/task/university.py
@@ -0,0 +1,8 @@
+def main():
+    marks = [int(input()) for _ in range(3)]
+    print(sum(marks) / len(marks))
+    print("Congratulations, you are accepted!")
+
+
+if __name__ == '__main__':
+    main()
