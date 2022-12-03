ad7fefe79e2997844ca828f04da6f27d85cefbc4
Viktor Reshetnikov <reshviktor1@gmail.com>
2022-12-03 15:22:09 +0200

    University Admission Procedure stage 3/7

--- SQL & python/University Admission Procedure/task/university.py
+++ SQL & python/University Admission Procedure/task/university.py
@@ -1,11 +1,12 @@
 def main():
-    marks = [int(input()) for _ in range(3)]
-    avg_score = sum(marks) / len(marks)
-    print(avg_score)
-    if avg_score < 60:
-        print("We regret to inform you that we will not be able to offer you admission.")
-    else:
-        print("Congratulations, you are accepted!")
+    total_number = int(input())
+    accepted = int(input())
+    list_of_candidates = iter(sorted([input().split(" ") for _ in range(total_number)],
+                                     key=lambda x: (-float(x[2]), x[0] + x[1])))
+    print("Successful applicants:")
+    for _ in range(accepted):
+        current = next(list_of_candidates)
+        print(current[0], current[1])
 
 
 if __name__ == '__main__':
