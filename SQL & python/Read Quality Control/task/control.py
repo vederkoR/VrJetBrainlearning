141260ec8afa963b6b64c5d9c511fb7c0cefe585
Viktor Reshetnikov <reshviktor1@gmail.com>
2022-12-05 15:39:14 +0200

    Read Quality Control 2/6

--- SQL & python/Read Quality Control/task/control.py
+++ SQL & python/Read Quality Control/task/control.py
@@ -1,10 +1,21 @@
+from collections import Counter
+
+
 def main():
     file_name = input()
+    sequences = []
     with open(file_name, mode='r') as file:
         for ind, row in enumerate(file):
-            print(row.strip())
-            if ind == 3:
-                break
+            if (ind - 1) % 4 != 0:
+                continue
+            sequences.append(len(row.strip()))
+        with_counts = sorted(list(Counter(sequences).items()), key=lambda x: x[0])
+        final_length = int(round(sum(sequences) / len(sequences), 0))
+        print(f"Reads in the file = {len(sequences)}:")
+        for k, v in with_counts:
+            print(f"\twith length {k} = {v}")
+
+        print(f"Reads sequence average length = {final_length}")
 
 
 if __name__ == '__main__':
