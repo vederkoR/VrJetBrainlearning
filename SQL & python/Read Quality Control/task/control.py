4d4b3d18680740b7b738c1d063240fb5d76835d4
Viktor Reshetnikov <reshviktor1@gmail.com>
2022-12-05 21:48:33 +0200

    Read Quality Control 3/6

--- SQL & python/Read Quality Control/task/control.py
+++ SQL & python/Read Quality Control/task/control.py
@@ -1,3 +1,4 @@
+import itertools
 from collections import Counter
 
 
@@ -8,14 +9,15 @@ def main():
         for ind, row in enumerate(file):
             if (ind - 1) % 4 != 0:
                 continue
-            sequences.append(len(row.strip()))
-        with_counts = sorted(list(Counter(sequences).items()), key=lambda x: x[0])
-        final_length = int(round(sum(sequences) / len(sequences), 0))
+            sequences.append(row.strip())
+        # with_counts = sorted(list(Counter([len(i) for i in sequences]).items()), key=lambda x: x[0])
+        all_nucls = list(itertools.chain(*sequences))
+        final_length = int(round(sum([len(i) for i in sequences]) / len(sequences), 0))
+        gc_contant_avg = sum([(i.count('C') + i.count('G')) / len(i) for i in sequences]) / len(sequences)
         print(f"Reads in the file = {len(sequences)}:")
-        for k, v in with_counts:
-            print(f"\twith length {k} = {v}")
-
         print(f"Reads sequence average length = {final_length}")
+        print()
+        print(f"GC content average = {gc_contant_avg:.2%}")
 
 
 if __name__ == '__main__':
