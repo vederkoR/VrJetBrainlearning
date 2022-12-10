3b768849e822eb468ad4448cbbcc219b0d1377d1
Viktor Reshetnikov <reshviktor1@gmail.com>
2022-12-10 03:09:31 +0200

    RPS game stage  2/5

--- SQL & python/Rock-Paper-Scissors/task/game.py
+++ SQL & python/Rock-Paper-Scissors/task/game.py
@@ -1,9 +1,18 @@
+import random
+
 loser_mapping = {'scissors': 'rock', 'paper': 'scissors', 'rock': 'paper'}
 
 
 def main():
-    choice = input()
-    print(f"Sorry, but the computer chose {loser_mapping[choice]}")
+    user_choice = input()
+    computer_choice = random.choice(['scissors', 'rock', 'paper'])
+    match (user_choice, computer_choice):
+        case ('scissors', 'rock') | ('paper', 'scissors') | ('rock', 'paper'):
+            print(f"Sorry, but the computer chose {computer_choice}")
+        case ('scissors', 'scissors') | ('paper', 'paper') | ('rock', 'rock'):
+            print(f"There is a draw ({computer_choice})")
+        case ('scissors', 'paper') | ('paper', 'rock') | ('rock', 'scissors'):
+            print(f"Well done. The computer chose {computer_choice} and failed")
 
 
 if __name__ == '__main__':
