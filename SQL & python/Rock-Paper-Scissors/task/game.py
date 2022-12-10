ceb0f09b5aa5b373f5878e1421ace2f9295230a9
Viktor Reshetnikov <reshviktor1@gmail.com>
2022-12-10 03:25:11 +0200

    RPS game stage  3/5

--- SQL & python/Rock-Paper-Scissors/task/game.py
+++ SQL & python/Rock-Paper-Scissors/task/game.py
@@ -1,18 +1,25 @@
 import random
 
-loser_mapping = {'scissors': 'rock', 'paper': 'scissors', 'rock': 'paper'}
+OPTIONS = ['scissors', 'rock', 'paper']
 
 
 def main():
-    user_choice = input()
-    computer_choice = random.choice(['scissors', 'rock', 'paper'])
-    match (user_choice, computer_choice):
-        case ('scissors', 'rock') | ('paper', 'scissors') | ('rock', 'paper'):
-            print(f"Sorry, but the computer chose {computer_choice}")
-        case ('scissors', 'scissors') | ('paper', 'paper') | ('rock', 'rock'):
-            print(f"There is a draw ({computer_choice})")
-        case ('scissors', 'paper') | ('paper', 'rock') | ('rock', 'scissors'):
-            print(f"Well done. The computer chose {computer_choice} and failed")
+    while True:
+        user_choice = input()
+        if user_choice == '!exit':
+            print('Bye!')
+            break
+        if user_choice not in OPTIONS:
+            print('Invalid input')
+            continue
+        computer_choice = random.choice(OPTIONS)
+        match (user_choice, computer_choice):
+            case ('scissors', 'rock') | ('paper', 'scissors') | ('rock', 'paper'):
+                print(f"Sorry, but the computer chose {computer_choice}")
+            case ('scissors', 'scissors') | ('paper', 'paper') | ('rock', 'rock'):
+                print(f"There is a draw ({computer_choice})")
+            case ('scissors', 'paper') | ('paper', 'rock') | ('rock', 'scissors'):
+                print(f"Well done. The computer chose {computer_choice} and failed")
 
 
 if __name__ == '__main__':
