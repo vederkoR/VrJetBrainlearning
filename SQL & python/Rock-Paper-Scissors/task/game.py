0d6ac03aebb31ae78a2a032cb65d241e63cf7723
Viktor Reshetnikov <reshviktor1@gmail.com>
2022-12-10 19:04:57 +0200

    RPS game stage 5/5 final

--- SQL & python/Rock-Paper-Scissors/task/game.py
+++ SQL & python/Rock-Paper-Scissors/task/game.py
@@ -1,24 +1,27 @@
 import random
 
-OPTIONS = ['scissors', 'rock', 'paper']
-flag_registered = True
-
 
 def find_or_set_user(user_name):
-    with open('rating.txt') as file:
-        for row in file:
-            if user_name in row:
-                return int(row.strip().split(" ")[1])
-    global flag_registered
-    flag_registered = False
+    try:
+        with open('rating.txt') as file:
+            for row in file:
+                if user_name in row:
+                    return int(row.strip().split(" ")[1])
+    except FileNotFoundError:
+        pass
     return 0
 
 
 def main():
     user_name = input("Enter your name:")
     score = find_or_set_user(user_name)
-    original_score = score
     print("Hello,", user_name)
+    options = input()
+    if not options:
+        options = 'scissors,rock,paper'
+    options = options.split(",")
+    print("Okay, let's start")
+
     while True:
         user_choice = input()
         if user_choice == '!exit':
@@ -27,20 +30,21 @@ def main():
         if user_choice == '!rating':
             print('Your rating:', score)
             break
-        if user_choice not in OPTIONS:
+        if user_choice not in options:
             print('Invalid input')
             continue
-        computer_choice = random.choice(OPTIONS)
-        match (user_choice, computer_choice):
-            case ('scissors', 'rock') | ('paper', 'scissors') | ('rock', 'paper'):
-                print(f"Sorry, but the computer chose {computer_choice}")
-                score += 0
-            case ('scissors', 'scissors') | ('paper', 'paper') | ('rock', 'rock'):
-                print(f"There is a draw ({computer_choice})")
-                score += 50
-            case ('scissors', 'paper') | ('paper', 'rock') | ('rock', 'scissors'):
-                print(f"Well done. The computer chose {computer_choice} and failed")
-                score += 100
+        computer_choice = random.choice(options)
+        index_of_user_chose = options.index(user_choice)
+        list_for_decision = options[index_of_user_chose + 1:] + options[:index_of_user_chose]
+        if computer_choice == user_choice:
+            print(f"There is a draw ({computer_choice})")
+            score += 50
+        elif list_for_decision.index(computer_choice) < len(list_for_decision) / 2:
+            print(f"Sorry, but the computer chose {computer_choice}")
+            score += 0
+        else:
+            print(f"Well done. The computer chose {computer_choice} and failed")
+            score += 100
 
 
 if __name__ == '__main__':
